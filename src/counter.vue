340939a046abfec596ec4bb22de290384c20eafe
DK-codeur <47182035+DK-codeur@users.noreply.github.com>
2021-02-12 09:15:11 +0000

    first commit

---
+++ src/counter.vue
@@ -0,0 +1,40 @@
+// mon composant
+<template>
+	<div class="counter">
+		 {{count}}
+     <div class="counter__bunton" @click="increment">+</div>
+	</div>
+
+</template>
+
+
+<script>
+  // import store from './store/notification_store'
+
+	export default {
+		data() {
+			return {
+        count: 0
+        // state: store.state
+			}
+    },
+
+    // computed: {
+    //   count: function () {
+    //     return this.state.count
+    //   }
+    // },
+
+		methods: {
+			increment () {
+        this.count++
+        // store.increment
+			}
+		},
+	}
+</script>
+
+
+<style src="./counter.css">
+
+</style>
---
+++ src/store/notification_store.js
@@ -0,0 +1,18 @@
+//class notificationStore
+class NotificationStore {
+	constructor() {
+		this.state = {
+			count: 0
+		}
+	}
+
+	increment() {
+		this.state.count++
+	}
+
+	decrement() {
+		this.state.count--
+	}
+}
+
+export default new NotificationStore()
